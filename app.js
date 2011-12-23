4358da9b85765899c756032403410bf6cce140fa
Jarvis Johnson <magicjarvis@gmail.com>
2011-12-22 22:33:45 -0500

    Fix port

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
