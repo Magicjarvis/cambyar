a1088e08499fe77b9e6640c78f43b2be7ec65a67
Jarvis Johnson <magicjarvis@gmail.com>
2011-12-27 13:03:27 -0500

    Add gravatar functionality

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@
  * GET home page.
  */
 var mongoose = require('mongoose');
+var md5 = require('MD5');
 var models = require('../lib/models')
 var User = models.User;
 
 exports.index = function(req, res){
     var name = 'Gangsta';
-    if(req.user) name = req.user.username;
+    var email_hash = 'http://www.gravatar.com/avatar/';
+    if(req.user) {
+        name = req.user.username;
+        email_hash+=md5(req.user.email);
+    }
     console.log(JSON.stringify(req.user));
     res.render('index', {  
         'name': name,
         'loggedIn': req.loggedIn,
+        'email_hash': email_hash,
     });
 };
