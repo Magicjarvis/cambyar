85d88852bfa19a7d7475e27de97fc52b1b46148e
Russell Cullen <russellscullen@gmail.com>
2011-12-29 15:56:43 -0500

    Add clickable name in topbar and check for taken email in profile change form

--- routes/user.js
+++ routes/user.js
@@ -44,45 +44,51 @@ exports.edit = function(req, res, next) {
 exports.update = function(req, res, next) {
     var interests = req.body.interests.split(',');
     var expertise = req.body.expertise.split(',');
-    async.map(interests, function(interest, cb) {
-        models.Tag.findOne({name: interest}, function(err, tag) {
-            if (err) cb(err, null);
-            else if (!tag) {
-                var new_tag = new models.Tag({name: interest});
-                new_tag.save(function(err){
-                    cb(null, new_tag._id);
-                });
-            }
-            else cb(null, tag._id);
-        });
-    }, function(err, interest_ids) {
+    models.User.findOne({'email': req.body.email}, function(err, user){
         if (err) return next(err);
-        async.map(expertise, function(subject, cb) {
-            models.Tag.findOne({name: subject}, function(err, tag) {
-                if (err) cb(err, null);
-                else if (!tag){
-                    var new_tag = new models.Tag({name: subject});
-                    new_tag.save(function(err){
-                        cb(null, new_tag._id);
-                    });
-                }
-                else cb(null, tag._id);
-            });
-        }, function(err, expert_ids) {
-            if (err) return next(err);
-            models.User.update({username: req.user.username}, {
-                'name': {
-                    'first': req.body.name_first,
-                    'last':  req.body.name_last,
-                },
-                'email': req.body.email,
-                'interests': interest_ids,
-                'expertise': expert_ids,
-            }, function (err){
+        if (user) res.redirect('user/edit-profile'); 
+        else {
+            async.map(interests, function(interest, cb) {
+                models.Tag.findOne({name: interest}, function(err, tag) {
+                    if (err) cb(err, null);
+                    else if (!tag) {
+                        var new_tag = new models.Tag({name: interest});
+                        new_tag.save(function(err){
+                            cb(null, new_tag._id);
+                        });
+                    }
+                    else cb(null, tag._id);
+                });
+            }, function(err, interest_ids) {
                 if (err) return next(err);
-                res.redirect('/user/'+req.user.username);
-            }); 
-        });
-    
+                async.map(expertise, function(subject, cb) {
+                    models.Tag.findOne({name: subject}, function(err, tag) {
+                        if (err) cb(err, null);
+                        else if (!tag){
+                            var new_tag = new models.Tag({name: subject});
+                            new_tag.save(function(err){
+                                cb(null, new_tag._id);
+                            });
+                        }
+                        else cb(null, tag._id);
+                    });
+                }, function(err, expert_ids) {
+                    if (err) return next(err);
+                    models.User.update({username: req.user.username}, {
+                        'name': {
+                            'first': req.body.name_first,
+                            'last':  req.body.name_last,
+                        },
+                        'email': req.body.email,
+                        'interests': interest_ids,
+                        'expertise': expert_ids,
+                    }, function (err){
+                        if (err) return next(err);
+                        res.redirect('/user/'+req.user.username);
+                    }); 
+                });
+            
+            });
+        }
     });
 }
